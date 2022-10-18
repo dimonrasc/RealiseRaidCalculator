33de4fa32fd964c0f6cb598460c8acb415b14f30
hu_f <hu_f@live.com>
2022-10-18 19:55:30 +0300

    Initial Commit

---
+++ RealiseRaidCalculator/RealiseRaidCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealiseRaidCalculatorApp.swift
+//  RealiseRaidCalculator
+//
+//  Created by huf on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct RealiseRaidCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
